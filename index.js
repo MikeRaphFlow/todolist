85e34cc79f35fa00c799bc09e4f84bd225f88d3f
MikeRaphFlow <miguerafael@gmail.com>
2023-10-10 17:20:05 -0300

    Agregado conexion a BD online

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/tododolistDB");
+mongoose.connect("mongodb+srv://miguerafael:anita9491@mikeraphcluster.rmnjhuu.mongodb.net/tododolistDB");
 
 const itemsSchema = new mongoose.Schema ({
     name: String    
